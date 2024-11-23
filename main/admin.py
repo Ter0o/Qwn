273b726763c823c466d323bb719154e8151bb884
Maksim Pleshkov <146361213+zein75@users.noreply.github.com>
2024-11-24 00:18:49 +0500

    Add some views and urls

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Category
+
+admin.site.register(Category)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+admin.site.register(Product)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'price',
+                    'image', 'available', 'created', 'updated', 'discount'
+                    ]
+    list_filter = ['available', 'created', 'updated', 'discount']
+
+    list_editable = ['price', 'available', 'discount']
+    prepopulated_fields = {'slug': ('name',)}
--- main/models.py
+++ main/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
 
     salary = models.DecimalField(max_digits=10, decimal_places=2)
 
-    availably = models.BooleanField(default=True)
+    available = models.BooleanField(default=True)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
