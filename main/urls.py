57f963f1a8150db4bd275c4f5a98301e9843e263
Maksim Pleshkov <146361213+zein75@users.noreply.github.com>
2024-12-01 01:15:26 +0500

    Add users and login system

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,7 @@ app_name = 'main'
 urlpatterns = [
     path('', views.popular_list, name='popular_list'),
     path('<slug:slug>/', views.product_detail, name='product_detail'),
-    path('errors', views.get_error, name='get_error'),
+    path('error505', views.get_error, name='get_error'),
+    path('error404', views.get_error1, name='get_error1'),
+    path('error202', views.get_error2, name='get_error2'),
 ]
--- qwn/urls.py
+++ qwn/urls.py
@@ -23,6 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
+    path('user/', include('users.urls', namespace='user')),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
